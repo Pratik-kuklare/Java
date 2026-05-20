86084c80fab42ea6885472af607957f69fbbd69b
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-05-20 11:58:40 +0530

    Java Vairables

---
+++ variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    
+    public static void main(String[] args) {
+        var name = "John";
+        var age = 30;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
