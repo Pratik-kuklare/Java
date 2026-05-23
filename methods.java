342026c2c92a7bcefe4d7acac82dd4aaedafe55d
Pratik-Kuklare <pratikkuklare1901@gmail.com>
2026-05-23 13:37:49 +0530

    ...

---
+++ methods.java
@@ -0,0 +1,28 @@
+public class methods {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+        calculateScore(true, 10000, 8, 200);
+
+        int finalScore = score;
+
+        if(gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+
+        if(gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    } 
+}
